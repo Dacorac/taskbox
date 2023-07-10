a1ad746c810df9f6b8708803a5694d3bacab161f
Daniela Córdoba <danielacordoba@Danielas-MacBook-Air.local>
2023-07-10 18:17:07 +1000

    First single and composed component Task and TaskList

---
+++ src/components/Task.stories.jsx
@@ -0,0 +1,52 @@
+import Task from './Task';
+import PropTypes from 'prop-types';
+
+export default {
+  component: Task,
+  title: 'Task',
+  tags: ['autodocs'],
+};
+
+export const Default = {
+  args: {
+    task: {
+      id: '1',
+      title: 'Test Task',
+      state: 'TASK_INBOX',
+    },
+  },
+};
+
+export const Pinned = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: 'TASK_PINNED',
+    },
+  },
+};
+
+export const Archived = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: 'TASK_ARCHIVED',
+    },
+  },
+};
+
+Task.propTypes = {
+  /** Composition of the task */
+  task: PropTypes.shape({
+    /** Id of the task */
+    id: PropTypes.string.isRequired,
+    /** Title of the task */
+    title: PropTypes.string.isRequired,
+    /** Current state of the task */
+    state: PropTypes.string.isRequired,
+  }),
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+};
